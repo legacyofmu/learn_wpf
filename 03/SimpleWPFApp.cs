c61ddc284b6431ca32b37b541f13dd3c5de9cb2b
legacyofmu <legacyofmu1@gmail.com>
2019-06-19 16:49:52 +0900

    Add Startup/Exit event

--- 03/SimpleWPFApp.cs
+++ 03/SimpleWPFApp.cs
@@ -10,14 +10,25 @@ namespace SimpleWPFApp
 		[STAThread]
 		static void Main()
 		{
+			MyWPFApp app = new MyWPFApp();
+			app.Startup += AppStartup;
+			app.Exit += AppExit;
+			app.Run();
 		}
 		
 		static void AppExit(object sender, ExitEventArgs e)
 		{
+			MessageBox.Show("App hax exited");
 		}
 		
 		static void AppStartup(object sender, StartupEventArgs e)
 		{
+			Window mainWindow = new Window();
+			mainWindow.Title = "My First WPF App!";
+			mainWindow.Height = 200;
+			mainWindow.Width = 300;
+			mainWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+			mainWindow.Show();
 		}
 	}
 }
