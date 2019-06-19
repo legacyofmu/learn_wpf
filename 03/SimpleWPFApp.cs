1495e511c906b5c8f4323f9cdcb7bff321295eab
legacyofmu <legacyofmu1@gmail.com>
2019-06-19 16:37:46 +0900

    Make '03' folder - Make WPF Applicaiton

---
+++ 03/SimpleWPFApp.cs
@@ -0,0 +1,23 @@
+//
+using System;
+using System.Windows;
+using System.Windows.Controls;
+namespace SimpleWPFApp
+{
+	//
+	class MyWPFApp : Application
+	{
+		[STAThread]
+		static void Main()
+		{
+		}
+		
+		static void AppExit(object sender, ExitEventArgs e)
+		{
+		}
+		
+		static void AppStartup(object sender, StartupEventArgs e)
+		{
+		}
+	}
+}
